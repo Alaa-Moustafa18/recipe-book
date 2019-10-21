b5154a7a78570fe90783a52e0053fe8af041072c
Alaa Hamed <ahamed@nbsventure.com>
2019-10-21 09:36:21 +0200

    add files

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { RecipesService } from './recipes.service';
+import { Recipe } from './recipe.model';
+import { map } from 'rxjs/operators'
+import { AuthService } from '../auth/auth.service';
+import * as firebase from 'firebase'
+@Injectable({
+  providedIn: 'root'
+})
+export class DataStorageService {
+
+  selectedFile: File = null
+  constructor(private http: HttpClient,
+    private recipesService: RecipesService,
+    private authService: AuthService,
+  ) { }
+
+ 
+  storeRecipe() {
+    return this.http.put("https://recipe-book-ec1dc.firebaseio.com/recipes.json", this.recipesService.getRecipes())
+  }
+  getRecipes() {
+    this.http.get<Recipe[]>("https://recipe-book-ec1dc.firebaseio.com/recipes.json")
+      .pipe(
+        map(
+          (recipes) => {
+            for (let recipe of recipes) {
+              if (!recipe['ingredients']) {
+                recipe['ingredients'] = []
+              }
+            }
+            return recipes
+          }
+        )
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipesService.setRecipes(recipes)
+        }
+      )
+  }
+}
